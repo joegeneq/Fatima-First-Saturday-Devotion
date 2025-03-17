d0b3a98d3d1479eed184125c00777531c1d1bd85
joegeneq <joegeneq@users.noreply.github.com>
2025-03-17 18:15:59 +0800

    moved tailwind.css

--- fatima-first-saturday/src/components/Home.vue
+++ fatima-first-saturday/src/components/Home.vue
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fatima First Saturday Devotion</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+   
 </head>
 <body class="bg-gray-100 text-gray-900">
     <!-- Navbar -->
@@ -114,7 +114,8 @@
 </html>
 </template>
 
-<script>
+
+ <script src="https://cdn.tailwindcss.com">
 
 </script>
 
